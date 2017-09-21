9d94cf1feeedaf93413f5e720a23efd1f0ed6702
Benny Siegert <bsiegert@gmail.com>
2017-09-21 19:20:52 +0200

    Some minimal function.
    New argument, -index, to print the index.
    Arguments are now checked if they are already packages, the packaging
    process itself remains a placeholder.

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
 var InfoLog = log.New(ioutil.Discard, "", log.LstdFlags)
 
 var (
+	index     = flag.Bool("index", false, "Print the reverse index of Go packages instead of adding any ports.")
 	verbose   = flag.Bool("v", true, "Print verbose messages about what is happening.")
 	pkgsrcdir = flag.String("pkgsrc", "", "Path to the top-level pkgsrc directory, will be taken from the PKGSRCDIR environment variable if not given.")
 )
@@ -28,21 +30,34 @@ func init() {
 }
 
 func main() {
-	/*if flag.NArg() == 0 {
+	flag.Parse()
+	if flag.NArg() == 0 && !*index {
 		log.Fatal("Need at least one argument")
 	}
-	for _, pkgpath := range flag.Args() {
-		if err := HandleURL(pkgpath); err != nil {
-			log.Fatal(err)
-		}
-	}*/
-	r, err := FullScan(*pkgsrcdir)
+
+	revIndex, err := FullScan(*pkgsrcdir)
 	if err != nil {
 		log.Fatal(err)
 	}
-	r.WriteTo(os.Stdout)
+	if *index {
+		revIndex.WriteTo(os.Stdout)
+		return
+	}
+	for _, goSrcPath := range flag.Args() {
+		if err := HandleURL(revIndex, goSrcPath); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
-func HandleURL(pkgpath string) error {
+func HandleURL(r ReverseIndex, srcpath string) error {
+	// Not a simple map lookup because of prefix matches
+	for src, pkg := range r {
+		if strings.HasPrefix(srcpath, src) {
+			log.Printf("%s is already part of a pkgsrc package (%s)", srcpath, pkg.Path)
+			return nil
+		}
+	}
+	log.Printf("This is where we would package %s", srcpath)
 	return nil
 }
