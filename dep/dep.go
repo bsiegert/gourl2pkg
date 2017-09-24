c6336531f0efb7afc55c80344beb656cf46e149f
Benny Siegert <bsiegert@gmail.com>
2017-09-24 16:59:35 +0200

    Discover which dependencies are standard library.

--- dep/dep.go
+++ dep/dep.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"go/build"
 	"log"
+	"os/exec"
 )
+
+var stdLib = stdLibPackages()
 
 func main() {
 	flag.Parse()
@@ -16,6 +20,33 @@ func main() {
 	}
 }
 
+// stdlibPackages returns the set of packages in the standard Go library.
+// The expansion of "std" is done inside the go tool, so shell out.
+func stdLibPackages() map[string]struct{} {
+	pkgs := make(map[string]struct{})
+	cmd := exec.Command("go", "list", "std")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	s := bufio.NewScanner(stdout)
+	for s.Scan() {
+		pkgs[s.Text()] = struct{}{}
+	}
+	if err := s.Err(); err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+
+	return pkgs
+}
+
 func ShowImports(srcpath string) error {
 	ctx := build.Default
 	// TODO set GOPATH
@@ -23,6 +54,19 @@ func ShowImports(srcpath string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Printf("Imports: %v\nTest Imports: %v\n", pkg.Imports, pkg.TestImports)
+	fmt.Println("Imports:")
+	printImports(pkg.Imports)
+	fmt.Println("Test imports:")
+	printImports(pkg.TestImports)
 	return nil
+}
+
+func printImports(imports []string) {
+	for _, imp := range imports {
+		std := ""
+		if _, ok := stdLib[imp]; ok {
+			std = " (std lib)"
+		}
+		fmt.Printf(" - %s%s\n", imp, std)
+	}
 }
